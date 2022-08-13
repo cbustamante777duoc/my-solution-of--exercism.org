6ef5deba8a8abc030d4b650055dbf2b43ebec386
cra.bustamante@alumnos.duoc.cl <Verdeamor+777>
2022-08-13 11:28:11 -0400

    e4 functions

---
+++ freeCoding/e3.js
@@ -0,0 +1,9 @@
+/**
+ * function that union two arrays
+ * @param {*} arr1 data array
+ * @param {*} arr2 data array
+ * @returns union two arrays
+ */
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
