7fcfa70e7f3452c8531e7b6757d8dc3b243dc5ea
cra.bustamante@alumnos.duoc.cl <Verdeamor+777>
2022-09-11 21:33:14 -0300

    ejercicio5

---
+++ basicos/ejer5.ts
@@ -0,0 +1,16 @@
+(()=>{
+
+
+    //funcion que le envia numero y te dice que tan largo es 
+
+    const largeOfNumber = (numero:Number) =>{
+
+        return `el largo del numero es ${numero.toString().length}`;
+    }
+
+    console.log(largeOfNumber(1000000));//el largo del numero es 7 
+
+
+    
+
+})()
