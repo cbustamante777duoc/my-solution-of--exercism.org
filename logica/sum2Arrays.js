e3ed3e1b74cc87e5decb2b5948f213a3761d095d
cra.bustamante@alumnos.duoc.cl <cra.bustamante@alumnos.duoc.cl>
2023-10-17 11:26:02 -0300

    sum 2 arrays

---
+++ logica/sum2Arrays.js
@@ -0,0 +1,31 @@
+(() => {
+
+    // Exercise 1:
+
+    // Write a JavaScript function that takes an array of numbers and returns the sum of all the numbers in the array. The function should use a nested for loop to iterate over the array.
+
+    /**
+  * Calculates the sum of all numbers in a 2D array.
+  * 
+  * @param {number[][]} array - The 2D array of numbers.
+  * @returns {number} - The sum of all numbers in the array.
+  */
+    function sumArray(array) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[i].length; j++) {
+                sum += array[i][j];
+            }
+        }
+        return sum;
+    }
+
+    const myArray = [[1, 2, 3], [4, 5, 6]];
+    const sum = sumArray(myArray);
+
+    console.log(sum);
+
+
+
+
+})
